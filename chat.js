86615000eed8a0f90dada6070ac2fa829f2c0339
Andrew Munsell <andrew@wizardapps.net>
2012-07-31 17:10:18 -0700

    Displaying messages

--- chat.js
+++ chat.js
@@ -1,3 +1,7 @@
+Messages = new Meteor.Collection('messages');
+
 if (Meteor.is_client) {
-  
+  Template.messages.messages = function(){
+    return Messages.find({}, { sort: { time: -1 }});
+  }
 }
